bf091ff10c0a77d7fe049efd34357c2fee6f8cb3
huangyixi <huangyixi2013@163.com>
2025-07-24 19:01:25 +0800

    增加部门信息

--- DjangoProjectDay16/urls.py
+++ DjangoProjectDay16/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 # from django.contrib import admin
 from django.urls import path
-
+from app01 import views
 urlpatterns = [
     #    path('admin/', admin.site.urls),
+    path('/depart/list/',views.depart_List),
 ]
--- app01/views.py
+++ app01/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from app01 import models
+
+
 # Create your views here.
+
+def depart_List(request):
+    '''部门列表'''
+
+    '''数据库中获取所有的部门列表'''
+    query_set = models.Department.objects.all()
+
+    return render(request,'depart_list.html',{'query_set':query_set})
