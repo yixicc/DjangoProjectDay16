9a43a9decb8330acea027340732b079ca86d9037
huangyixi <huangyixi2013@163.com>
2025-07-30 14:43:32 +0800

    使用无法新增部门的问题

--- app01/views.py
+++ app01/views.py
@@ -20,7 +20,7 @@ def depart_add(request):
     if request.method == 'GET':
         return render(request,'depart_add.html')
 
-    departname = request.POST.get("departname")
+    departname = request.POST.get("title")
     models.Department.objects.create(title=departname)
     return redirect('/depart/list/')
 
