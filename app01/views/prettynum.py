af128bebe20dfe4ef86b5bd839826026b6ccdf3c
huangyixi <huangyixi2013@163.com>
2025-08-04 14:14:58 +0800

    靓号列表增加分页

--- app01/views/prettynum.py
+++ app01/views/prettynum.py
@@ -8,6 +8,7 @@ from django.utils.safestring import mark_safe
 
 from app01 import models
 from app01.utils.form import PrettyNumModelForm, PrettyNumEditModelForm
+from app01.utils.pagination import Pagination
 
 
 def prettynum_list(request):
@@ -17,75 +18,16 @@ def prettynum_list(request):
     if search_word:
         search_dict["mobile__contains"] = search_word
 
-    #根据用户想要访问的页面计算起止位置
-    page = int(request.GET.get('page',1))
+    queryset = models.PrettyNum.objects.filter(**search_dict).order_by("-level")
+    page_object = Pagination(request, queryset)
+    context = {
+        "search_data": search_word,
 
-    page_size = 10
-    start = (page - 1) * page_size
-    end = page * page_size
+        "queryset": page_object.page_queryset,  # 分完页的数据
+        "page_string": page_object.html()  # 页码
+    }
 
-    #   数据总条数
-    total_count = models.PrettyNum.objects.filter(**search_dict).count()
-    # 总页码数
-    total_pages,div = divmod(total_count,page_size)
-    if div:
-        total_pages = total_pages + 1
-
-    # 计算当前页的前5页和后5页
-    plus = 5
-    # 总页数小于11页
-    if total_pages <= 2 * plus + 1 :
-        start_page = 1
-        end_page = total_pages
-    else:
-        # 总页数大于11页
-        if page <= plus:
-            # 当前页小于5
-            start_page = 1
-            end_page = 2 * plus + 1
-        elif page >= total_pages - plus:
-            # 当前页大于5
-            start_page = page - plus
-            end_page = total_pages
-        else:
-            start_page = page - plus
-            end_page = page + plus
-
-    #页码
-    page_str_list = []
-
-    #首页
-    page_str_list.append('<li class="active"><a href="?page={}">首页</a></li>'.format(1))
-
-    # 上一页
-    if page > 1:
-        ele = '<li class="active"><a href="?page={}">上一页</a></li>'.format(page - 1)
-    else:
-        ele = '<li class="active"><a href="?page={}">上一页</a></li>'.format(1)
-    page_str_list.append(ele)
-
-    for i in range (start_page, end_page + 1):
-        if i == page:
-            ele = '<li class="active"><a href="?page={}">{}</a></li>'.format(i,i)
-        else:
-            ele = '<li><a href="?page={}">{}</a></li>'.format(i,i)
-        page_str_list.append(ele)
-
-    # 下一页
-    if page < total_pages:
-        ele = '<li class="active"><a href="?page={}">下一页</a></li>'.format(page + 1)
-    else:
-        ele = '<li class="active"><a href="?page={}">下一页</a></li>'.format(total_pages)
-    page_str_list.append(ele)
-
-    #尾页
-    page_str_list.append('<li class="active"><a href="?page={}">尾页</a></li>'.format(total_pages))
-
-    page_string = mark_safe("".join(page_str_list))
-
-    '''数据库中获取所有的靓号列表'''
-    query_set = models.PrettyNum.objects.filter(**search_dict).order_by("-level")[start:end]
-    return render(request,'prettynum_list.html',{'query_set':query_set,'search_value':search_word,"page_string":page_string})
+    return render(request,'prettynum_list.html',{'context':context})
 
 def prettynum_add(request):
     if request.method == 'GET':
