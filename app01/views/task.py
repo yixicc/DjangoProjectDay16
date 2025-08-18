c61d130e282971feabbd88192642482253e892cc
huangyixi <huangyixi2013@163.com>
2025-08-18 18:43:33 +0800

    增加ajax应用示例

--- app01/views/task.py
+++ app01/views/task.py
@@ -3,16 +3,21 @@
 # @Author : zhou
 # @File : task.py
 # @Software: PyCharm
+import json
+
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 
 def task_list(request):
     '''任务列表'''
     return render(request, 'task_list.html')
 
-
+@csrf_exempt
 def task_ajax(request):
     print("request.GET",request.GET)
-    return HttpResponse("成功了")
+    print("request.POST",request.POST)
+    data_dict = {"status":"success","data":[11,22,33]}
+    return HttpResponse(json.dumps(data_dict))
 
